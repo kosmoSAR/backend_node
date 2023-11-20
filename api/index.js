0bd1e702253f7f8d6fabafdd06d7fe7e9f8453bd
kosmoSAR <santiagoaristizabal.kosmetikon@gmail.com>
2023-11-20 13:47:10 -0500

    implementaciín de vercel

--- api/index.js
+++ api/index.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const routerApi = require('./routes');
+const cors = require('cors');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //MiddleWare
 app.use(express.json());
 
+//Cors
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('No permitido'))
+    }
+  }
+}
+app.use(cors(options));
+
 //Primera ruta con respuesta, usamos request(req) y response(res).
-app.get("/", (req, res) =>{
+app.get("/api", (req, res) =>{
   res.send("Hola mi server en Express");
 });
 
--- api/routes/productsRouter.js
+++ api/routes/productsRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const ProductService = require('./../services/product.service');
-const validatorHandler = require('./../middlewares/validator.handler');
+const validatorHandler = require('../api/middlewares/validator.handler');
 const { createProductSchema, updateProductSchema, getProductSchema } = require('./../schemas/product.schemas');
 
 const router = express.Router();
